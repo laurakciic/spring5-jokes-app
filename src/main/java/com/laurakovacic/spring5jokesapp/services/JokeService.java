6c446e33b58be7dcc75d5404268ed43af77fc7f5
Laura <laurakovacic@outlook.com>
2023-01-27 22:55:29 +0100

    implement JokeService. Closes #1

---
+++ src/main/java/com/laurakovacic/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,8 @@
+package com.laurakovacic.spring5jokesapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/laurakovacic/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.laurakovacic.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
